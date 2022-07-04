4a7f52571e90582085b49c15a1989232f36e4259
wgoins6 <wgoins6@gatech.edu>
2022-07-04 05:34:26 -0400

    Serve index.html to root

--- myApp.js
+++ myApp.js
@@ -4,7 +4,9 @@ let app = express();
 console.log("Hello World");
 
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+//    res.send('Hello Express');
+    let absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
 })
 
 
